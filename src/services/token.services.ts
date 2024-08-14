c4862d4b407806f2113a57df47974f109ca21e59
KEVIN RIMACHI <rimachi.kevin7@gmail.com>
2024-08-14 09:36:56 -0500

    JWT -- Modificamos la hora de expiracón del token

--- src/services/token.services.ts
+++ src/services/token.services.ts
@@ -6,6 +6,6 @@ const JWT_SECRET = process.env.JWT_SECRET || "my_secret_key%%%";
 
 export const generateToken = (user_auth: Cliente | Trabajador): string => {
   return jwt.sign({ id: user_auth.id, email: user_auth.email }, JWT_SECRET, {
-    expiresIn: "2h",
+    expiresIn: "24h",
   });
 };
